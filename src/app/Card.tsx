dce77585792f560c8eaad498d63b84d60e9140dc
kumawabh <kumawabh@WL350183>
2021-06-19 13:02:19 +0530

    persist favourite state

--- src/app/Card.tsx
+++ src/app/Card.tsx
@@ -1,6 +1,8 @@
 import { getPhotoUrl } from './integration/http-client';
 
 import './Card.scss';
+import { useEffect, useState } from 'react';
+import { isFavourite, setFavourite, removeFavourite } from './integration/favourite';
 
 export default function({ serverId, photoId, secret, size, title, owner }
                                 : {
@@ -13,23 +15,57 @@ export default function({ serverId, photoId, secret, size, title, owner }
                                 }) {
     const url = getPhotoUrl(serverId, photoId, secret, size);
 
+    const [hover, setHover] = useState<boolean>(false);
+    const [favourite, setfavourite] = useState<boolean>(false);
+
+    useEffect(() => {
+        const _ = isFavourite(serverId, photoId);
+        setfavourite(_);
+    }, [favourite]);
+
+    const mouseEnter = () => {
+        setHover(true);
+    };
+
+    const mouseLeave = () => {
+        setHover(false);
+    };
+
+    const makeFavourite = () => {
+        if (favourite) {
+            removeFavourite(serverId, photoId);
+        } else {
+            setFavourite(serverId, photoId);
+        }
+        
+        setfavourite(!favourite);
+    };
+
+    const overlay = () => {
+        return (<div className="overlay">
+                    <div className="favourite-outer-container">
+                        <div className="favourite-container">
+                            <div className="title">{title}</div>
+                            <div className="line-break"> </div>
+                            <div className="owner">{owner}</div>
+                            <button className="favourite-btn"
+                                onClick={() => makeFavourite()}>
+                                    { favourite ? 'Unfavourite' : 'Favourite' }
+                                </button>
+                        </div>
+                    </div>
+                </div>);
+    };
+
     return (
-        <div className="card-outer-container">
+        <div className="card-outer-container" onMouseEnter={() => mouseEnter()}
+        onMouseLeave={() => mouseLeave()}>
             <div className="card-inner-container">
                 <picture>
                     <img src={url} alt="" />
                 </picture>
             </div>
-            <div className="overlay">
-                <div className="favourite-outer-container">
-                    <div className="favourite-container">
-                        <div className="title">{title}</div>
-                        <div className="line-break"> </div>
-                        <div className="owner">{owner}</div>
-                        <button className="favourite-btn">Favourite</button>
-                    </div>
-                </div>
-            </div>
+            { hover && overlay() }
         </div>
     );
 }
---
+++ src/app/integration/favourite.ts
@@ -0,0 +1,49 @@
+const STORAGE_KEY = 'photos';
+
+interface Favourite {
+    serverId: string;
+    photoId: string;
+}
+
+const setFavourite = (serverId: string, photoId: string): void => {
+    const value = sessionStorage.getItem(STORAGE_KEY);
+    if (value) {
+        let favourites = JSON.parse(value) as Favourite[];
+        favourites = [...favourites, { serverId, photoId }];
+
+        sessionStorage.setItem(STORAGE_KEY, JSON.stringify(favourites));
+    } else {
+        let favourites: Favourite[] = [{ serverId, photoId }];
+
+        sessionStorage.setItem(STORAGE_KEY, JSON.stringify(favourites));
+    }
+}
+
+const removeFavourite = (serverId: string, photoId: string): void => {
+    const value = sessionStorage.getItem(STORAGE_KEY);
+    if (value) {
+        let favourites = JSON.parse(value) as Favourite[];
+        const index = favourites.findIndex((f) => f.serverId === serverId && f.photoId === photoId);
+        if(index !== -1) {
+            favourites.splice(index, 1);
+
+            sessionStorage.setItem(STORAGE_KEY, JSON.stringify(favourites));
+        }
+    }
+}
+
+const isFavourite = (serverId: string, photoId: string): boolean => {
+    const value = sessionStorage.getItem(STORAGE_KEY);
+    if (value) {
+        let favourites = JSON.parse(value) as Favourite[];
+        if (favourites && favourites.length > 0) {
+            return favourites.some((f) => f.serverId === serverId && f.photoId === photoId);
+        }
+
+        return false;
+    }
+
+    return false;
+}
+
+export { setFavourite, removeFavourite, isFavourite };
