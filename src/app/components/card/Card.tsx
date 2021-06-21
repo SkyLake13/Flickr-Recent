783bc2b9813f9d946f2928fd0e6c92e33bcfe311
kumawabh <kumawabh@WL350183>
2021-06-21 19:11:12 +0530

    Using css to show/hide overlay instead of element mouse event, resulting in less re-renders

--- src/app/components/card/Card.tsx
+++ src/app/components/card/Card.tsx
@@ -1,9 +1,7 @@
 import { getPhotoUrl } from '../../integration/flickr-client';
-import { useState } from 'react';
-
-import './Card.scss';
 import { useFavourite } from './useFavourite';
 
+import './Card.scss';
 interface CardProp {
     serverId: string, 
     photoId: string, 
@@ -16,44 +14,33 @@ interface CardProp {
 function Card({ serverId, photoId, secret, size, title, owner }: CardProp) {
     const url = getPhotoUrl(serverId, photoId, secret, size);
 
-    const [hover, setHover] = useState<boolean>(false);
     const [favourite, toggleFavourite] = useFavourite(serverId, photoId);
-
-    const mouseEnter = () => {
-        setHover(true);
-    };
-
-    const mouseLeave = () => {
-        setHover(false);
-    };
 
     const makeFavourite = () => {
         toggleFavourite();
     };
 
-    const overlay = () => {
-        return (<div className="overlay">
-                    <div className="favourite-container">
-                        <div className="title">{title}</div>
-                        <hr className="line-break"/>
-                        <div className="owner">{owner}</div>
-                        <button className="favourite-btn"
-                            onClick={() => makeFavourite()}>
-                                { favourite ? 'Unfavourite' : 'Favourite' }
-                            </button>
-                    </div>
-                </div>);
-    };
+    const overlay = 
+         (<div className="overlay">
+            <div className="favourite-container">
+                <div className="title">{title}</div>
+                <hr className="line-break"/>
+                <div className="owner">{owner}</div>
+                <button className="favourite-btn"
+                    onClick={() => makeFavourite()}>
+                        { favourite ? 'Unfavourite' : 'Favourite' }
+                    </button>
+            </div>
+        </div>);
 
     return (
-        <div className="card-outer-container" onMouseEnter={() => mouseEnter()}
-        onMouseLeave={() => mouseLeave()}>
+        <div className="card-outer-container">
             <div className="card-inner-container">
                 <picture>
-                    <img src={url} alt="" />
+                    <img src={url} alt={title} />
                 </picture>
             </div>
-            { hover && overlay() }
+            { overlay }
         </div>
     );
 }
